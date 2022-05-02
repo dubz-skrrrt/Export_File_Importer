0882dc43dbf96ad88b05456913105636d183892c
dubz-skrrrt <chill.99.lr@gmail.com>
2022-05-02 15:07:09 +0800

    created table

--- Excel_MySQL importer.py
+++ Excel_MySQL importer.py
@@ -5,7 +5,7 @@ import xlrd
 
 try:
     connection = mysql.connector.connect(host="localhost",
-                                         database="ExcelImporter",
+                                         database="ExcelFileImporter",
                                          user="root",
                                          password="qwerty1234")
     if connection.is_connected():
@@ -15,6 +15,19 @@ try:
         cursor.execute("select database();")
         record = cursor.fetchone()
         print("You're connected to database: ", record)
+
+        personal_details_table_Query = """CREATE TABLE IF NOT EXISTS personal_details (Id int(11) NOT NULL,
+                                                                        photo varchar(250) NOT NULL,
+                                                                        fname varchar(250) NOT NULL,
+                                                                        mname varchar(250) NOT NULL,
+                                                                        lname varchar(250) NOT NULL,
+                                                                        member_since DATE NOT NULL,
+                                                                        reg_data DATE NOT NULL,
+                                                                        exp_data DATE NOT NULL,
+                                                                        is_active int(5) NOT NULL,
+                                                                        PRIMARY KEY (Id)) """
+        result = cursor.execute(personal_details_table_Query)
+        print("Personal Details Table created successfully ")
 except Error as e:
     print("Error while connecting to MySQL", e)
 
