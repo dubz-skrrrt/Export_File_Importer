59e2aeb9798f89d359f8e1a3cbb58e433ace488f
dubz-skrrrt <chill.99.lr@gmail.com>
2022-05-02 14:41:48 +0800

    connecting mysql db

--- Excel_MySQL importer.py
+++ Excel_MySQL importer.py
@@ -1,7 +1,26 @@
-import pymysql
-pymysql.install_as_MySQLdb()
-import MySQLdb
+import mysql.connector
+from mysql.connector import Error
 import xlrd
 
-database = MySQLdb.connect(host="localhost", user="root", passwd="qwerty1234", db="mysql")
-cursor = database.cursor()
+
+try:
+    connection = mysql.connector.connect(host="localhost",
+                                         database="ExcelImporter",
+                                         user="root",
+                                         password="qwerty1234")
+    if connection.is_connected():
+        db_info = connection.get_server_info()
+        print("Connected to MySQL server version ", db_info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+except Error as e:
+    print("Error while connecting to MySQL", e)
+
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+
+#personal_details_table = "CREATE TABLE IF NOT EXISTS personal_details(id int)"
